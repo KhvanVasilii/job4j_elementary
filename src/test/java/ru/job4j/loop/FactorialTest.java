d79d07fe87d84110db7b16aa90c838cd8970a387
KhvanVasilii <hvanvasya@gmail.com>
2022-02-14 05:29:55 +0900

    deleted comment

--- src/test/java/ru/job4j/loop/FactorialTest.java
+++ src/test/java/ru/job4j/loop/FactorialTest.java
@@ -17,7 +17,6 @@ public class FactorialTest {
 
     @Test
     public void whenCalculateFactorialForZeroThenOne() {
-        /* напишите здесь тест, проверяющий, что факториал для числа 0 равен 1. */
         int n = 0;
         int expected = 1;
         int out = Factorial.calc(n);
