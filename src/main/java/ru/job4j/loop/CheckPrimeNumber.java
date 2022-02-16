417632c19301998821af598b812ceb62630c408d
KhvanVasilii <hvanvasya@gmail.com>
2022-02-16 18:43:08 +0900

    refactor

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -3,6 +3,7 @@ package ru.job4j.loop;
 public class CheckPrimeNumber {
 
     public static boolean check(int number) {
+
         for (int i = 2; i < number; i++) {
             if (number % i == 0) {
                 return false;
@@ -12,6 +13,7 @@ public class CheckPrimeNumber {
     }
 
     public static int calc(int finish) {
+
         int count = 0;
         for (int number = 2; number <= finish; number++) {
             if (CheckPrimeNumber.check(number)) {
