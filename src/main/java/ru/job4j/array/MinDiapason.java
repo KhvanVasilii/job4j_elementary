871ebd775826aa44794c8d2ce40b061d8c491ac1
KhvanVasilii <hvanvasya@gmail.com>
2022-02-19 20:41:10 +0900

    refactor

--- src/main/java/ru/job4j/array/MinDiapason.java
+++ src/main/java/ru/job4j/array/MinDiapason.java
@@ -3,9 +3,9 @@ package ru.job4j.array;
 public class MinDiapason {
     public static int findMin(int[] array, int start, int finish) {
         int min = array[start];
-        for (start++; start <= finish; start++) {
-            if (min > array[start]) {
-                min = array[start];
+        for (int i = start + 1; i <= finish; i++) {
+            if (min > array[i]) {
+                min = array[i];
             }
         }
         return min;
